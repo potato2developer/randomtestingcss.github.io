d1e9d7d9a100ec5a8564167ea95c3219515c66cd
potato2developer <107358340+potato2developer@users.noreply.github.com>
2022-09-01 16:14:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ function pos(argument) {
 	var postop = Math.floor(Math.random() * 200) +100 ;
 	title.setAttribute("style",  "color: red; position: relative; left:" + posleft +"px;" +"top:"
 	+ postop + "px;" );// style setting 
-console.log(posleft+"left pos");
-console.log(postop+"top pos");
+console.log("Title:"+"left position: " + posleft + " top position: "+postop);
+}
+
+function block(argument){
+	var block3 = document.getElementById("block2");
+	var posleftblock = Math.floor(Math.random() * 300) +1 ;//random numbers from 200 to 300 pos
+	var postopblock = Math.floor(Math.random() * 50) +1 ;
+	var info = block3.setAttribute("style",  "width:100px; height:100px; background-color: red; position: relative; left:"
+	 + posleftblock +"px;"+"top: "+postopblock+"px;"); //block settings 
+	console.log(posleftblock);
 }
